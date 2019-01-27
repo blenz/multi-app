21cf6c9d4ab105c4e91fb47da0db9f4943451e04
blenz <bbluegga@yahoo.com>
2019-01-26 17:33:08 -0800

    remove log, add header link css

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -12,20 +11,19 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Calculate Fibonaci Number</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-        </div>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+          <header className="header">
+              <h1 className="App-title">Calculate Fibonacci Number</h1>
+              <Link to="/">Home</Link>
+              <Link to="/otherpage">Other Page</Link>
+            </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </Router>
     );
-  }
+  };
 }
 
 export default App;
